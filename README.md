cdd43948d35162530e87f48305f22a15aba79d5f
Wagon Wheel <70062069+njleoni@users.noreply.github.com>
2020-11-20 21:15:44 -0700

    readme build

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
 
 # readme_generator
-https://github.com/njleoni/readme_generator
+
+## Desription
+
+This is a story about a girl who found her way tot he cookie factory and then she ate all the cookies and got a belly ache.
+
+## Table of Contents
+ - [Installation](#installation)
+ - [Usage](#usage)
+ - [License](#license)
+ - [Contributing](#contributing)
+ - [Tests](#tests)
+ - [Questions](#questions)
+
+## Installation
+    
+## Usage
+
+## License
+
+## Contributing
+
+## Tests
+
+## Questions
+[GitHub Repo](https://github.com/njleoni/readme_generator)
+#### nicholas.leoni@gmail.com
+
     
--- script.js
+++ script.js
@@ -4,52 +4,58 @@ inquirer
     .prompt([
         {
             type: 'input',
-            message: 'What is your name?',
-            name: 'name',
+            message: 'What is your repo title?',
+            name: 'title',
+        },    
+        {
+            type: 'input',
+            message: 'What is your  GitHub user name?',
+            name: 'gitUser',
         },
         {
             type: 'input',
-            message: 'Where are you from?',
-            name: 'location',
+            message: 'What is your  email address?',
+            name: 'email',
         },
         {
             type: 'input',
-            message: 'What is is your GitHub username?',
-            name: 'GitHub',
+            message: 'Give me a short description of your project?',
+            name: 'info',
         },
-        {
-            type: 'input',
-            message: 'What LinkedIn URL?',
-            name: 'LinkedIn',
-        }
     ])
     .then((data) => {
-        const userName = `
-    <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <title>Document</title>
-</head>
-<body>
-    <div class="jumbotron" style="background-color: lightskyblue;">
-            <h1 class="display-4">Hi! My name is ${data.name}</h1>
-            <p class="lead">I am from ${data.location}</p>
-            <hr class="my-4">
-            <h2>Contact Me</h2>
-            <h6>My GitHub username is: ${data.GitHub}</h6>
-            <h6>LinkedIn: ${data.LinkedIn}</h6>  
-    </div>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-    <script type="text/javascript" src="index.js"></script>
-</body>
-</html>
+        const redmeFile = `
+# ${data.title}
+
+## Desription
+
+${data.info}
+
+## Table of Contents
+ - [Installation](#installation)
+ - [Usage](#usage)
+ - [License](#license)
+ - [Contributing](#contributing)
+ - [Tests](#tests)
+ - [Questions](#questions)
+
+## Installation
+    
+## Usage
+
+## License
+
+## Contributing
+
+## Tests
+
+## Questions
+[GitHub Repo](https://github.com/${data.gitUser}/${data.title})
+#### ${data.email}
+
     `;
-        const fileName = `${data.name.toLowerCase().split(" ").join("")}.json`;
-        fs.writeFile('index.html', userName, (err) =>
+        // const fileName = `${data.name.toLowerCase().split(" ").join("")}.json`;
+        fs.writeFile('README.md', redmeFile, (err) =>
             err ? console.log(err) : console.log("success!")
         );
     });
