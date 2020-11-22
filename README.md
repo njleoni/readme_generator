20c256480340e1d364f3efbf40b6f65f25a3f7c8
Wagon Wheel <70062069+njleoni@users.noreply.github.com>
2020-11-21 22:46:22 -0700

    updates

--- README.md
+++ README.md
@@ -1,30 +1,27 @@
 
 # readme_generator
-
+MIT License [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 ## Desription
-
-This is a story about a girl who found her way tot he cookie factory and then she ate all the cookies and got a belly ache.
+ read me generator
 
 ## Table of Contents
  - [Installation](#installation)
  - [Usage](#usage)
  - [License](#license)
- - [Contributing](#contributing)
+ - [Contribute](#contribute)
  - [Tests](#tests)
  - [Questions](#questions)
 
 ## Installation
-    
+node script.js    
 ## Usage
-
+answer questions
 ## License
-
+MIT License [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 ## Contributing
-
+i did
 ## Tests
-
+node script.js
 ## Questions
 [GitHub Repo](https://github.com/njleoni/readme_generator)
 #### nicholas.leoni@gmail.com
-
-    
--- script.js
+++ script.js
@@ -1,61 +1,123 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: 'What is your repo title?',
-            name: 'title',
-        },    
-        {
-            type: 'input',
-            message: 'What is your  GitHub user name?',
-            name: 'gitUser',
-        },
-        {
-            type: 'input',
-            message: 'What is your  email address?',
-            name: 'email',
-        },
-        {
-            type: 'input',
-            message: 'Give me a short description of your project?',
-            name: 'info',
-        },
-    ])
-    .then((data) => {
-        const redmeFile = `
+
+function start() {
+
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is your repo title?',
+                name: 'title',
+            },    
+            {
+                type: 'input',
+                message: 'What is your GitHub user name?',
+                name: 'gitUser',
+            },
+            {
+                type: 'input',
+                message: 'What is your email address?',
+                name: 'email',
+            },
+            {
+                type: 'input',
+                message: 'Give me a short description of your project?',
+                name: 'info',
+            },
+            {
+                type: 'input',
+                message: 'What are the installation instructions?',
+                name: 'install',
+            },
+            {
+                type: 'input',
+                message: 'How is it used?',
+                name: 'usage',
+            },
+            {
+                type: 'input',
+                message: 'Who contributed to this project?',
+                name: 'contribute',
+            },
+            {
+                type: 'input',
+                message: 'What testing was performed?',
+                name: 'test',
+            },
+            {
+                type: 'checkbox',
+                message: 'What licenses were used?',
+                name: 'license',
+                choices: ['Apache License 2.0', 'GNU GPLv3', 'ISC License', 'MIT License [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'],
+            },            
+
+        ])
+    
+        .then((data) => {
+            writeInfo(data);
+
+                })  
+}
+
+
+function writeInfo(data) {
+    switch (data.license) {
+      case "Apache License 2.0":
+        licenseImage =
+          "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+        break;
+      case "GNU GPLv3":
+        licenseImage =
+          "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+        break;
+      case "ISC License":
+        licenseImage =
+          "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)";
+        break;
+      case "MIT License":
+        licenseImage =
+          "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+        break;
+      default:
+        break;
+        
+    } 
+
+    console.log(data.license);
+    const redmeFile = `
 # ${data.title}
-
+${data.license}
 ## Desription
-
-${data.info}
+ ${data.info}
 
 ## Table of Contents
  - [Installation](#installation)
  - [Usage](#usage)
  - [License](#license)
- - [Contributing](#contributing)
+ - [Contribute](#contribute)
  - [Tests](#tests)
  - [Questions](#questions)
 
 ## Installation
-    
+${data.install}    
 ## Usage
-
+${data.usage}
 ## License
-
+${data.license}
 ## Contributing
-
+${data.contribute}
 ## Tests
-
+${data.test}
 ## Questions
 [GitHub Repo](https://github.com/${data.gitUser}/${data.title})
 #### ${data.email}
+`;
 
-    `;
-        // const fileName = `${data.name.toLowerCase().split(" ").join("")}.json`;
-        fs.writeFile('README.md', redmeFile, (err) =>
-            err ? console.log(err) : console.log("success!")
-        );
-    });
+    fs.writeFile('README.md', redmeFile, (err) =>
+        err ? console.log(err) : console.log("success!")
+    )
+}
+
+start();
+
